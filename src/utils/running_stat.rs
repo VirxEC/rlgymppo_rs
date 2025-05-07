9662fd6e3dc12bdc3dde198e9f4b383130b3b084
VirxEC <virx@virxcase.dev>
2025-05-07 20:20:50 +0000

    Use old running stat

--- src/utils/running_stat.rs
+++ src/utils/running_stat.rs
@@ -12,18 +12,21 @@ pub struct Stats {
 
 #[derive(Serialize, Deserialize, Debug, Default, Clone, Copy)]
 pub struct WelfordRunningStat {
-    mean: f64,
-    n: usize,
-    m2: f64,
+    running_mean: f64,
+    running_variance: f64,
+    count: usize,
 }
 
 impl WelfordRunningStat {
     fn update(&mut self, sample: f64) {
-        self.n += 1;
-        let delta = sample - self.mean;
-        self.mean += delta / self.n as f64;
-        let delta2 = sample - self.mean;
-        self.m2 += delta * delta2;
+        let current_count = self.count as f64;
+        self.count += 1;
+
+        let delta = sample - self.running_mean;
+        let delta_n = delta / self.count as f64;
+
+        self.running_mean += delta_n;
+        self.running_variance += delta * delta_n * current_count;
     }
 
     pub fn increment(&mut self, samples: Vec<f32>) {
@@ -33,11 +36,11 @@ impl WelfordRunningStat {
     }
 
     pub fn get_std(&self) -> f32 {
-        if self.n < 2 {
+        if self.count < 2 {
             return 1.0;
         }
 
-        let cur_var = self.m2 / (self.n - 1) as f64;
+        let cur_var = self.running_variance / (self.count - 1) as f64;
         if cur_var == 0.0 {
             1.0
         } else {

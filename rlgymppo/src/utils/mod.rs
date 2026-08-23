40487480d87a9187a955a32f8d1a5b65bff4e36c
Ted Zards <tjado@edzards.eu>
2026-08-23 17:22:34 +0200

    Make trainer example only have rlgymppo as dependency

--- rlgymppo/src/utils/mod.rs
+++ rlgymppo/src/utils/mod.rs
@@ -1,4 +1,6 @@
-pub use rlgymppo_utils::{actions, obs, rewards, shared_info, state_setters, terminal};
+pub use rlgymppo_utils::{
+    AvgTracker, Report, actions, obs, rewards, shared_info, state_setters, terminal,
+};
 
 pub(crate) mod running_stat;
 pub mod serde;
